6352f533160cba57dca2f2ffa053fbc40ee4540a
VanModers <49339876+VanModers@users.noreply.github.com>
2022-05-06 10:43:12 +0200

    Improved some methods

--- src/Studiengang.java
+++ src/Studiengang.java
@@ -3,13 +3,14 @@ import java.util.List;
 import java.util.Objects;
 
 public class Studiengang implements Comparable<Studiengang> {
-    private String bezeichnung;
-    private int sws;
-    private List<Modul> module = new ArrayList<>();
+    private final String bezeichnung;
+    private final int sws;
+    private final List<Modul> module;
 
     public Studiengang(String bezeichnung, int sws) {
         this.bezeichnung = bezeichnung;
         this.sws = sws;
+        this.module = new ArrayList<>();
     }
 
     public String getBezeichnung() {
@@ -31,7 +32,7 @@ public class Studiengang implements Comparable<Studiengang> {
     @Override
     public int compareTo(Studiengang o) {
         if(sws != o.sws)
-            return ((Integer)sws).compareTo(o.sws);
+            return Integer.compare(sws, o.sws);
         return bezeichnung.compareTo(o.bezeichnung);
     }
 
